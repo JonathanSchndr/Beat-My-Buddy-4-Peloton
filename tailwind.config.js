4713496671a64bd649b01e25ec7dcf02e7d157fd
Jonathan Schneider <jonathan@jott.media>
2022-02-20 14:56:13 +0100

    fix lint error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   mode: "jit",
-  purge: [
+  content: [
     "./components/**/*.{vue,js}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
+    "./app.vue",
     "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
     extend: {},
   },
