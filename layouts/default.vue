e3284db2b06f72d60c7fd82d9c758d1442d6476d
Jonathan Schneider <jonathan@jott.media>
2022-02-20 18:57:27 +0100

    add pinia and modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="min-h-full">
+    <TheLogin />
     <TheHeader />
     <div class="py-10">
       <div class="max-w-3xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
