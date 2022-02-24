7cd65fa350be50f2a84c56f80765a27ffa77714c
Jonathan Schneider <jonathan@jott.media>
2022-02-24 20:53:55 +0100

    add route user

--- components/Element/Search.vue
+++ components/Element/Search.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="relative z-40 max-w-xl mx-auto transition-all transform bg-white divide-y divide-gray-100 shadow-2xl rounded-t-xl ring-1 ring-black ring-opacity-5" :class="{ 'rounded-b-xl': this.value.length < 4 }">
+  <div class="relative z-40 max-w-xl mx-auto transition-all transform bg-white divide-y divide-gray-100 shadow-2xl rounded-t-xl ring-1 ring-black ring-opacity-5" :class="{ 'rounded-b-xl': value.length < 4 }">
     <div class="relative">
       <svg class="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
       </svg>
-      <input v-model="value" type="text" class="w-full h-12 pr-4 text-gray-800 placeholder-gray-400 bg-transparent border-0 pl-11 focus:ring-0 sm:text-sm" placeholder="Username search..." role="combobox" aria-expanded="false" aria-controls="options" @keyup="searching()">
+      <input v-model="value" type="text" class="w-full h-12 pr-4 text-gray-800 placeholder-gray-400 bg-transparent border-0 pl-11 focus:ring-0 sm:text-sm" placeholder="Username search..." role="combobox" aria-expanded="false" aria-controls="options" @keyup="searching()" @keyup.enter="submit()">
     </div>
 
     <div v-if="value.length > 3" class="absolute z-50 w-full bg-white shadow-2xl rounded-b-xl">
-      <ul v-if="users.total > 0" class="p-3 overflow-y-auto max-h-96 scroll-py-3" id="options" role="listbox">
-        <li v-for="user in users.data" :key="user.id" class="flex p-3 cursor-pointer select-none hover:bg-gray-100 group rounded-xl" id="option-1" role="option" tabindex="-1">
-          <img :src="user.image_url" class="w-10 h-10 rounded-full" />
-          <div class="flex-auto ml-4">
-            <p class="text-sm font-medium text-gray-700">{{ user.username }}</p>
-            <p class="text-sm text-gray-500">Peloton ID: {{ user.id }}</p>
-          </div>
+      <ul v-if="users.total > 0" class="p-3 overflow-y-auto max-h-96 scroll-py-3">
+        <li v-for="user in users.data" :key="user.id">
+          <nuxt-link :to="`/user/${user.username}`" class="flex p-3 cursor-pointer select-none hover:bg-gray-100 group rounded-xl">
+            <img :src="user.image_url" class="w-10 h-10 rounded-full" />
+            <div class="flex-auto ml-4">
+              <p class="text-sm font-medium text-gray-700">{{ user.username }}</p>
+              <p class="text-sm text-gray-500">Peloton ID: {{ user.id }}</p>
+            </div>
+          </nuxt-link>
         </li>
       </ul>
 
@@ -55,6 +57,11 @@ export default {
           console.log(data)
         });
       }
+    },
+    submit() {
+      this.$router.push({path: `/user/${this.value}`});
+      this.value = ''
+      this.users = { data: [], total: 0 }
     }
   }
 }
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,13 +1,15 @@
 <template>
   <header class="bg-white shadow-sm">
     <div class="flex items-center justify-between p-4 mx-auto space-x-4 max-w-7xl sm:px-6 lg:px-8">
-      <img class="block w-8" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=rose&shade=500" alt="Beat my Buddy 4 Peloton">
+      <nuxt-link to="/">
+        <img class="block w-8" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=rose&shade=500" alt="Beat my Buddy 4 Peloton">
+      </nuxt-link>
       <ElementSearch class="w-full" />
       <div>
         <img class="w-8 rounded-full cursor-pointer" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" @click="toggleMenu()">
         <div v-if="menuOpen" class="absolute z-40 py-1 mt-3 bg-white rounded-md shadow-lg -translate-x-2/3 whitespace-nowrap ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">Your Profile</a>
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">Settings</a>
+          <!-- <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">Your Profile</a>
+          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">Settings</a> -->
           <button class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" @click="logout()">Sign out</button>
         </div>
       </div>
