343aa8f923b98ff2daeec0df4a21a4e825228fb7
Jonathan Schneider <jonathan@jott.media>
2022-02-24 22:16:11 +0100

    add netlify deploy

--- pages/user/_username.vue
+++ pages/user/_username.vue
@@ -39,7 +39,7 @@
                   <span class="flex-1 w-0 ml-2 truncate"> resume_back_end_developer.pdf </span>
                 </div>
                 <div class="flex-shrink-0 ml-4">
-                  <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Download </a>
+                  <a href="#" class="font-medium text-rose-600 hover:text-rose-500"> Download </a>
                 </div>
               </li>
               <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
@@ -51,7 +51,7 @@
                   <span class="flex-1 w-0 ml-2 truncate"> coverletter_back_end_developer.pdf </span>
                 </div>
                 <div class="flex-shrink-0 ml-4">
-                  <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Download </a>
+                  <a href="#" class="font-medium text-rose-600 hover:text-rose-500"> Download </a>
                 </div>
               </li>
             </ul>
@@ -61,7 +61,18 @@
     </div>
   </div>
   <div v-else>
-    404
+
+    <div class="py-16">
+      <div class="text-center">
+        <p class="text-sm font-semibold tracking-wide uppercase text-rose-600">404 error</p>
+        <h1 class="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">User not found.</h1>
+        <p class="mt-2 text-base text-gray-500">Sorry, we couldn’t find the user you’re looking for.</p>
+        <div class="mt-6">
+          <a href="/" class="text-base font-medium text-rose-600 hover:text-rose-500">Go back home<span aria-hidden="true"> &rarr;</span></a>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
